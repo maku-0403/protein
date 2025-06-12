76c4d0a8dba2831d477f22bcef25166f7bd904e7
Mahan Kunii <mahankunii0403@gmail.com>
2025-06-12 15:13:57 +0900

    簡略化２

--- for_graph/cif_csv_test.py
+++ for_graph/cif_csv_test.py
@@ -4,7 +4,7 @@ import glob
 
 w_res = list()
 degree = int(input('How many degree(s)? : '))
-a_number = int(input('How many number(s) of amino acid? : '))
+input_amino_number = int(input('How many number(s) of amino acid? : '))
 
 # mmCIFルートディレクトリ（あなたの環境に合わせて変更）
 root_dir = "/Volumes/pdb_res/CIF/cif_to_csv/all_csv_cosw"
@@ -28,7 +28,6 @@ counts = [[0 for _ in range(8)] for _ in range(8)]
 process_count = 0
 
 for csv_path in csv_files:
-    # 処理中のパスを表示
     process_count += 1
     if process_count % 100 == 0:
         print(f"処理中: {process_count} / {len(csv_files)}")
@@ -38,23 +37,33 @@ for csv_path in csv_files:
         if len(l[0]) == 2:
             file_name = l[0][1]
             count = 0
-            if len(l) - 6 >= a_number:
-                for j in range(6, len(l)):
-                    if float(l[j][3]) > degree:
-                        count += 1
-                w_rate = count / (len(l) - 6) * 100
-                if l[1][1] != '.' and l[1][1] != 'unknown':
-                    if float(l[1][1]) < 5:
-                        temp = [float(l[1][1]), w_rate, file_name, l[3][1]]
-                        w_res = temp
-            for i, (low, high) in enumerate([(0.5, 1.0), (1.0, 1.5), (1.5, 2.0), (2.0, 2.5), (2.5, 3.0), (3.0, 3.5), (3.5, 4.0), (4.0, float('inf'))]):
-                if low <= w_res[0] < high:
-                    for j, (low_rate, high_rate) in enumerate([(0, 5), (5, 10), (10, 15), (15, 20), (20, 40), (40, 60), (60, 80), (80, 100)]):
-                        if low_rate <= w_res[1] < high_rate:
-                            counts[i][j] += 1
-                            with open(f"{path_name}/each_PDBid/{save_path_pool[i]}{save_file_pool[j]}.csv", 'a') as f:
-                                writer = csv.writer(f)
-                                writer.writerow([w_res[2]])
+            multi_unit_name = [l[6][0]]
+            multi_unit_number = [6]
+            for i in range(7,len(l)):
+                if l[i][0] != l[i-1][0]:
+                    multi_unit_name.append(l[i][0])
+                    multi_unit_number.append(i)
+            multi_unit_number.append(len(l))
+            for i in range(0,len(multi_unit_name)):
+                unit_name = multi_unit_name[i]
+                amino_number = multi_unit_number[i+1] - multi_unit_number[i]
+                if  amino_number >= input_amino_number:
+                    for j in range(multi_unit_number[i], multi_unit_number[i+1]):
+                        if float(l[j][3]) > degree:
+                            count += 1
+                    w_rate = count / (amino_number - 6) * 100
+                    if l[1][1] != '.' and l[1][1] != 'unknown':
+                        if float(l[1][1]) < 5:
+                            temp = [float(l[1][1]), w_rate, file_name, l[3][1]]
+                            w_res = temp
+                for i, (low, high) in enumerate([(0.5, 1.0), (1.0, 1.5), (1.5, 2.0), (2.0, 2.5), (2.5, 3.0), (3.0, 3.5), (3.5, 4.0), (4.0, float('inf'))]):
+                    if low <= w_res[0] < high:
+                        for j, (low_rate, high_rate) in enumerate([(0, 5), (5, 10), (10, 15), (15, 20), (20, 40), (40, 60), (60, 80), (80, 100)]):
+                            if low_rate <= w_res[1] < high_rate:
+                                counts[i][j] += 1
+                                with open(f"{path_name}/each_PDBid/{save_path_pool[i]}{save_file_pool[j]}.csv", 'a') as f:
+                                    writer = csv.writer(f)
+                                    writer.writerow([w_res[2],unit_name])
 
 # 合計を計算
 totals = [sum(count) for count in counts]
@@ -76,5 +85,3 @@ with open(f"{path_name}/CSV_data.csv", 'w') as f:
     for j, save_file in enumerate(save_file_pool):
         writer.writerow([save_file] + [rates[i][j] for i in range(8)])
     writer.writerow(['sum'] + totals)
-
-print(len(w_res))
