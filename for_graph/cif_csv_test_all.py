b115699bbeb259e2b2627de70c8c5ae0fa16f4f1
Mahan Kunii <mahankunii0403@gmail.com>
2025-11-20 11:21:46 +0900

    グラフデータ生成プログラムに任意の設定とディレクトリの入力を実装

--- for_graph/cif_csv_test_all.py
+++ for_graph/cif_csv_test_all.py
@@ -4,17 +4,27 @@ import glob
 import pandas as pd
 
 # CSVを読み込み（ヘッダーなし）
-df = pd.read_csv("/Volumes/pdb_res/PDB/pdb_resolution_list.csv", header=None)
+df = pd.read_csv("/srv/shared/pdb_resolution_list.csv", header=None)
 
 # 1列目をキー、2列目を値として辞書に変換
 mapping = pd.Series(df[1].values, index=df[0]).to_dict()
 
 w_res = list()
-degree = int(input('How many degree(s)? : '))
-input_amino_number = int(input('How many number(s) of amino acid? : '))
+
+while(True):
+    input_setting = input("1:RUN 2:setting_params (initial:10 degrees, 100 amino number) : ")
+
+    if input_setting == '1':
+        degree = 10
+        input_amino_number = 100
+        break
+    elif input_setting == '2':
+        degree = int(input('How many degree(s)? : '))
+        input_amino_number = int(input('How many number(s) of amino acid? : '))
+        break
 
 # mmCIFルートディレクトリ（あなたの環境に合わせて変更）
-root_dir = "/Volumes/pdb_res/CIF/cif_to_csv/all_csv_cosw"
+root_dir = input("CIF root directory: ")
 
 # 全ての.cifファイルのフルパスを再帰的に取得
 csv_files = glob.glob(os.path.join(root_dir, '**', '*.csv'), recursive=True)
@@ -22,7 +32,7 @@ csv_files = glob.glob(os.path.join(root_dir, '**', '*.csv'), recursive=True)
 save_path_pool = ['0.5-1.0Å', '1.0-1.5Å', '1.5-2.0Å', '2.0-2.5Å', '2.5-3.0Å', '3.0-3.5Å', '3.5-4.0Å', '4.0-4.5Å', '4.5-5.0Å', '5.0Å-']
 save_file_pool = ['0-5%', '5-10%', '10-15%', '15-20%', '20-40%', '40-60%', '60-80%', '80-100%']
 
-path_name = '/Volumes/pdb_res/CIF/csv_to_graph_data/all_csv_resolution'
+path_name = input("output directory: ")
 
 os.makedirs("path/to/each_PDBid", exist_ok=True)
 for i in range(0,len(save_file_pool)):
