eb7eac9d33fca5b1882d17cb990a250b9c751b6b
Mahan Kunii <mahankunii0403@gmail.com>
2025-12-01 16:44:51 +0900

    軽い修正

--- for_graph/cif_csv_test_upper.py
+++ for_graph/cif_csv_test_upper.py
@@ -4,7 +4,7 @@ import glob
 import pandas as pd
 
 # CSVを読み込み（ヘッダーなし）
-df = pd.read_csv("/Volumes/pdb_res/PDB/pdb_resolution_list.csv", header=None)
+df = pd.read_csv("/srv/shared/pdb_resolution_list.csv", header=None)
 
 # 1列目をキー、2列目を値として辞書に変換
 mapping = pd.Series(df[1].values, index=df[0]).to_dict()
